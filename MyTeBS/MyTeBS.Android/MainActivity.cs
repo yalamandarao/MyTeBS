1e16b1ca077e39bd05faa49851eb1b3c3f016c84
Yalamandarao <yalamandarao.i@gmail.com>
2019-03-22 10:09:39 +0800

    - IOS Splash screen

--- MyTeBS/MyTeBS.Android/MainActivity.cs
+++ MyTeBS/MyTeBS.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MyTeBS.Droid
 {
-  [Activity(Label = "MyTeBS", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+  [Activity(Label = "MyTeBS", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
   public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
   {
     protected override void OnCreate(Bundle savedInstanceState)
